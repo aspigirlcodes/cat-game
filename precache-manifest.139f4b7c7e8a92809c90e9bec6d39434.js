628c791841f2d1da9c78a4aa93527775c1cc6e78
Sara <sara.peeters@gmail.com>
2020-05-27 20:56:14 +0200

    Updates

---
+++ precache-manifest.139f4b7c7e8a92809c90e9bec6d39434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b975c3e7799988bd5b444b5463a20839",
+    "url": "/cat-game/index.html"
+  },
+  {
+    "revision": "bf45b98f8728654aaa63",
+    "url": "/cat-game/static/css/main.0c07f884.chunk.css"
+  },
+  {
+    "revision": "cfd4f6261d8414404183",
+    "url": "/cat-game/static/js/2.1944103e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-game/static/js/2.1944103e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf45b98f8728654aaa63",
+    "url": "/cat-game/static/js/main.61537419.chunk.js"
+  },
+  {
+    "revision": "c09f02f0edd2ad64483e",
+    "url": "/cat-game/static/js/runtime-main.7819273d.js"
+  }
+]);
